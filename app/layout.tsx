2592110b5d829f8f4bb3ec8f919cfa423f567dcb
michaelorina <orinamichael4@gmail.com>
2023-04-26 18:26:31 +0300

    use font in body

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
